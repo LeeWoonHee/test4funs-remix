2db37a5a84b2d2f866d517b5cce61d2728bec1f9
LeeWoonHee <zero1158@naver.com>
2025-07-14 11:51:08 +0700

    메인 페이지 텍스트 제거

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -30,12 +30,12 @@ export default function Index() {
 
   return (
     <div className='w-full h-full'>
-      <div className='flex justify-center items-center text-[1.4vw] font-[700] pt-[4vw] text-[#00000f]'>
+      {/* <div className='flex justify-center items-center text-[1.4vw] font-[700] pt-[4vw] text-[#00000f]'>
         남녀노소 모두가 즐길 수 있는 다양한 주제의 퀴즈게임을 즐겨보세요!
       </div>
       <div className='flex justify-center items-center text-[1vw] font-[600] pt-[1vw] text-[#00000f]'>
         원하는 게임을 선택해서 시작하세요!
-      </div>
+      </div> */}
       <div className='px-[2vw]'>
         <MotionCardList data={examList} />
       </div>
